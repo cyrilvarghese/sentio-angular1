1e867cdb7298e2118646b39e3e2dd55e9a49bdc6
cyrilfosengage <cyril@fosengage,com>
2016-11-02 21:05:28 +0530

    plan change fix

--- src/app/billing/change-billing.controller.js
+++ src/app/billing/change-billing.controller.js
@@ -11,7 +11,7 @@
 
         // vm.currentPlan = organizationService.getCurrentOrganization().plan;
         vm.currentPlanId = organizationService.getCurrentOrganization().plan.plan_id || 0;
-        vm.currentPlanName = organizationService.getCurrentOrganization().plan.name || "trial";
+        vm.currentPlanName = organizationService.getCurrentOrganization().plan.plan_name || "trial";
         vm.paymentAllowed = userService.getCurrentUser().verified === 1;
         vm.userId = userService.getCurrentUser().user_id;
         vm.userEmail=userService.getCurrentUser().email;
